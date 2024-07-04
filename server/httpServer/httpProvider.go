ff28587eb0b2d73565b42ba5b8e0c94846adc770
rbell <randy@belltribe.us>
2024-07-04 09:07:57 -0600

    lint

--- server/httpServer/httpProvider.go
+++ server/httpServer/httpProvider.go
@@ -17,8 +17,6 @@ import (
 	"github.com/rbell/toolchest/server/serverConfig"
 )
 
-type httpServerOption func(server *http.Server)
-
 type HttpProvider struct {
 	httpSrver *http.Server
 	router    *httprouter.Router
